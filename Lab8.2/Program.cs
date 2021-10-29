217bb52876d29c210ee5a16a07c7b069109883d2
Tik88 <xtikx@yandex.ru>
2021-10-29 15:20:41 +0300

    Добавьте файлы проекта.

---
+++ Lab8.2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Lab8._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "Random.txt";
+            StreamWriter sw = new StreamWriter(path, true);
+            Random random = new Random();
+            for (int i = 0; i < 10; i++)
+            {
+                sw.WriteLine(random.Next(100));
+            }
+            sw.Close();
+            using (StreamReader sr = new StreamReader(path, System.Text.Encoding.Default))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);                    
+                }
+                int[] intArrFile = File.ReadAllLines(path).Select(int.Parse).ToArray();
+                int sum = intArrFile.Sum();
+                Console.WriteLine(sr.ReadToEnd());
+                Console.WriteLine("Сумма чисел: {0}", sum);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+
+namespace Lab8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dirName = @"C:\Users\b.kuzkin\Desktop\Работа";
+
+            if (Directory.Exists(dirName))
+            {
+                Console.WriteLine("Подкаталоги:");
+                string[] dirs = Directory.GetDirectories(dirName);
+                foreach (string s in dirs)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);
+                foreach (string s in files)
+                {
+                    Console.WriteLine(s);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
